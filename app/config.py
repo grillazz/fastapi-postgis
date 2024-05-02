fa02018b8781b51846dfb056699196dbd24bec61
Jakub Miazek <the@grillazz.com>
2024-05-02 12:03:45 +0200

    fix typo

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="fram.env", env_ignore_empty=True, extra="ignore"
+        env_file="farm.env", env_ignore_empty=True, extra="ignore"
     )
 
     SQL_USER: str
